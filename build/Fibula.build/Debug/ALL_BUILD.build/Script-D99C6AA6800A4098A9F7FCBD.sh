6073df97c791b0a8e59473f5d13b61f228f51fe6
Daniel Ribeiro <drgomesp@gmail.com>
2016-05-26 13:30:42 +0400

    Removed more trash

--- build/Fibula.build/Debug/ALL_BUILD.build/Script-D99C6AA6800A4098A9F7FCBD.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/drgomesp/ClionProjects/Fibula/build -f /Users/drgomesp/ClionProjects/Fibula/build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- build/Fibula.build/Debug/ZERO_CHECK.build/Script-CF24180B559D4329BEDEE166.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/drgomesp/ClionProjects/Fibula/build -f /Users/drgomesp/ClionProjects/Fibula/build/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
